93ab382bf1890e28ad8450516bd06fe2b3b10fe2
Chidiebee <camuchionu3@gmail.com>
2024-01-30 15:42:17 +0100

    Duck typing - first element of a sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
-""" Annotates function params and return values """
+"""Augment the following code with the correct duck-typed annotations:
 
-from typing import Sequence, Union, Any
+# The types of the elements of the input are not know
+def safe_first_element(lst):
+    if lst:
+        return lst[0]
+    else:
+        return None
+
+{'lst': typing.Sequence[typing.Any], 'return': \
+    typing.Union[typing.Any, NoneType]}
+"""
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """ Duck-typing """
+import typing
+
+
+def safe_first_element(lst: typing.Sequence[typing.Any]) -> \
+        typing.Union[typing.Any, None]:
+    """Duck-typed annotation"""
     if lst:
         return lst[0]
     else:
